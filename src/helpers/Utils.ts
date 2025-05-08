57329e7d0ea8bdc78125c140bd93b06086b7edfe
Jeremy Zongker <jeremy@zongker.net>
2025-05-07 20:46:06 -0500

    0.2.1 - Expo

--- src/helpers/Utils.ts
+++ src/helpers/Utils.ts
@@ -1,10 +1,19 @@
-import Snackbar from "react-native-snackbar";
+import * as Notifications from 'expo-notifications';
 import { StyleConstants } from "./StyleConstants";
+import * as Haptics from 'expo-haptics';
 
 export class Utils {
-  
+
   public static snackBar(message: string) {
-    Snackbar.show({ text: message, backgroundColor: StyleConstants.baseColor, duration: Snackbar.LENGTH_SHORT });
+    // For now, we'll just use haptic feedback since we're in a shared library
+    // The main app can implement its own notification system
+    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+    console.log(message);
+  }
+
+  static showMessage(message: string, duration: number = 3000) {
+    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+    console.log(message);
   }
 
 }
