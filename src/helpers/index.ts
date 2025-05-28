2f68284fc0b6683b9bfe810223a8e540d2b4fd1d
Jeremy Zongker <jeremy@zongker.net>
2025-05-28 13:38:43 -0500

    Removed expo incompatible helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,11 +1,9 @@
 export * from "@churchapps/helpers";
 export { FirebaseAnalyticsHelper } from "./AppCenterHelper";
 export { DeviceInfo } from "./DeviceInfo";
-export { DimensionHelper } from "./DimensionHelper";
 export { ErrorHelper } from "./ErrorHelper";
 export { FirebaseHelper } from "./FirebaseHelper";
 export * from "./Interfaces"
 export { PushNotificationHelper, pushEventBus } from "./PushNotificationHelper";
 export { StyleConstants } from "./StyleConstants";
-export { Utils } from "./Utils";
 export { ValidationHelper } from "./ValidationHelper";
